97824db74918e23ed72efa6988b946dddef0e34f
quydang <hadesi1999@gmail.com>
2020-10-06 12:17:34 +0700

    hahaha

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -53,11 +53,10 @@
                                 <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laravel.com/docs" class="underline text-gray-900 dark:text-white">Documentation</a></div>
                             </div>
 
-                            <div class="ml-12">
-                                <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    Laravel has wonderful, thorough documentation covering every aspect of the framework. Whether you are new to the framework or have previous experience with Laravel, we recommend reading all of the documentation from beginning to end.
-                                </div>
-                            </div>
+                        <div>
+                            Laravel New
+                        </div>   
+
                         </div>
 
                         <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
